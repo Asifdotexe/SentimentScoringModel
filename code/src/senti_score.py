7d7a85614946407931ffd11b74dec0afc6f8c307
Asif Sayyed <asifdotexe@gmail.com>
2024-06-17 11:52:10 +0530

    #17 created function for data_preprocessing()

--- code/src/senti_score.py
+++ code/src/senti_score.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import List
+from string import punctuation
+from nltk.tokenize.casual import casual_tokenize
+from src.nlp_preprocessing import clean_html_column
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from src.nlp_preprocessing import get_lemmas
 
 def read_data(file_path: str) -> pd.DataFrame:
     """
@@ -38,8 +44,8 @@ def read_data(file_path: str) -> pd.DataFrame:
     
     return df
 
-def class_rebalancing(df: pd.DataFrame, rating_column: str, export_path: str,
-                      sample_size: List[int] = [3_000, 1_000, 1_000, 1_000, 3_000], seed: int = 42) -> None:
+def class_rebalancing(df: pd.DataFrame, rating_column: str,
+                      sample_size: List[int] = [3_000, 1_000, 1_000, 1_000, 3_000], seed: int = 42) -> pd.DataFrame:
     """
     This function performs class rebalancing on a given DataFrame based on a specified rating column.
     It samples the data for each rating category according to the provided sample sizes and concatenates them into a new DataFrame.
@@ -48,13 +54,11 @@ def class_rebalancing(df: pd.DataFrame, rating_column: str, export_path: str,
     Parameters:
     - df (pd.DataFrame): The input DataFrame containing the data to be rebalanced.
     - rating_column (str): The name of the column in the DataFrame that contains the rating values.
-    - export_path (str): The path to the CSV file where the rebalanced data will be exported.
-    - sample_size (List[int], optional): A list of integers representing the desired sample sizes for each rating category.
-                                        Defaults to [3_000, 1_000, 1_000, 1_000, 3_000].
+    - sample_size (List[int], optional): A list of integers representing the desired sample sizes for each rating category. Defaults to [3_000, 1_000, 1_000, 1_000, 3_000].
     - seed (int, optional): A random seed for reproducibility. Defaults to 42.
 
     Returns:
-    - None: This function does not return any value, but it modifies the input DataFrame and exports the rebalanced data to a CSV file.
+    - pd.DataFrame: The rebalanced DataFrame after sampling data for each rating category.
 
     Raises:
     - ValueError: If the rating column is not found in the input DataFrame.
@@ -68,4 +72,45 @@ def class_rebalancing(df: pd.DataFrame, rating_column: str, export_path: str,
     five_star = df[df[rating_column] == rating_value[4]].sample(sample_size[4], random_state=seed)
 
     undersampled_df = pd.concat([one_star, two_star, three_star, four_star, five_star], axis=0)
-    undersampled_df.to_csv(export_path, index=False)
+    return undersampled_df
+    
+def data_preprocessing(df: pd.DataFrame, review_column: str, text_processing_method: str='stem') -> pd.DataFrame:
+    """
+    This function performs data preprocessing on a given DataFrame based on a specified review column.
+    It cleans the HTML from the review column, converts the text to lowercase, removes punctuation, and tokenizes the text.
+    Depending on the text_processing_method parameter, it either applies stemming or lemmatization to the tokens.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame containing the data to be preprocessed.
+    - review_column (str): The name of the column in the DataFrame that contains the review text.
+    - text_processing_method (str, optional): A string representing the text processing method to be applied. Defaults to 'stem'.
+
+    Returns:
+    - pd.DataFrame: The preprocessed DataFrame after applying the specified text processing method.
+
+    Raises:
+    - ValueError: If the review_column is not found in the input DataFrame.
+    - ValueError: If the text_processing_method is not 'stem' or 'lemma'.
+    """
+    df = clean_html_column(df, review_column, 'clean_review')
+
+    df['clean_review'] = df['clean_review'].apply(lambda review:
+        str(review).translate(str.maketranslate('', '', punctuation)).lower())
+
+    df['tokens'] = df['clean_review'].apply(lambda review:
+        casual_tokenize(str(review)))
+
+    if text_processing_method == 'stem':
+        stemmer = PorterStemmer()
+        df['stemmed_tokens'] = df['tokens'].apply(lambda words:
+            [stemmer.stem(word) for word in words])
+    elif text_processing_method == 'lemma':
+        lemmatizer = WordNetLemmatizer()
+        df['lemma_tokens'] = df['tokens'].apply(lambda tokens:
+            get_lemmas(tokens))
+    else:
+        raise ValueError(f"Unsupported text processing method: {text_processing_method}")
+
+    return df
+        
+        
